b8027e3d23fb89334dd8e31037500d8367f89f50
gabriela2393 <gabymoncayozerga23@gmail.com>
2019-03-01 01:33:08 -0500

    pasando función a cipher y aplicando en cajas de texto

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,39 @@
 window.cipher = {
   encode: (textToEncode,encodeOffset) => {
-    /* Acá va tu código */
-    
+    let valueOfTextToEncode = textToEncode.value; //A
+    let offset= parseInt(encodeOffset.value);
+
+    let palabra = "";
+
+ for(let i=0; i < valueOfTextToEncode.length ; i++) {
+   const getAsciiCode= valueOfTextToEncode.charCodeAt(i);
+   if (getAsciiCode>=65 && getAsciiCode<=90) {
+     let aplyingFormula= (getAsciiCode-65+ offset)%26 + 65;
+     const getEncodeWord = String.fromCharCode(aplyingFormula);
+     palabra = palabra + getEncodeWord;
+   }
+ }
+
+return palabra;
+
   },
-  decode: (textToEncode,decodeOffset) => {
-    /* Acá va tu código */
-  }
-};
+
+  decode: (textToDecode,decodeOffset) => {
+    let valueOfTextToDecode = textToDecode.value; //A
+    let offset= parseInt(decodeOffset.value);
+
+   let palabra = "";
+    for(let i=0; i < valueOfTextToDecode.length ; i++) {
+      const getAsciiCode= valueOfTextToDecode.charCodeAt(i);
+      if (getAsciiCode>=65 && getAsciiCode<=90) {
+        let aplyingFormula= (getAsciiCode-90 -offset)%26 + 90;
+        const getDecodeWord = String.fromCharCode(aplyingFormula);
+        palabra = palabra + getDecodeWord;
+      }
+    }
+
+   return palabra;
+ },
+
+
+ }
