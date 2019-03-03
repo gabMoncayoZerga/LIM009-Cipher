435b167bc20064b797484aa1c9ccb9601358336a
gabriela2393 <gabymoncayozerga23@gmail.com>
2019-03-03 00:25:33 -0500

    añadiendo test para cifrar y descifrar espacios

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -1,31 +1,29 @@
-//CIFRAR//
 describe('cipher', () => {
+ it('debería ser un objeto', () => {
+   assert.equal(typeof cipher, 'object');
+ });
+});
 
-  it('debería ser un objeto', () => {
-    assert.equal(typeof cipher, 'object');
-  });
+describe('cipher.encode', () => {
+ it('debería ser una función', () => {
+   assert.equal(typeof cipher.encode, 'function');
+ });
+ it("debería retornar 'HIJKLMNOPQRSTUVWXYZABCDEFG' para 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' con offest 33", () => {
+   assert.equal(cipher.encode(33,"ABCDEFGHIJKLMNOPQRSTUVWXYZ"), "HIJKLMNOPQRSTUVWXYZABCDEFG");
+ });
+ it("debería retornar 'H%I' para 'A(espacio)B' con offest 33", () => {
+   assert.equal(cipher.encode(33,"A B"),"H%I");
+ });
+});
 
-  describe('cipher.encode', () => {
-
-    it('debería ser una función', () => {
-      assert.equal(typeof cipher.encode, 'function');
-    });
-
-    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33', () => {
-        const result = window.cipher.encode('ABCDEFGHIJKLMNOPQRSTUVWXYZ',33);
-        assert.equal(result,"HIJKLMNOPQRSTUVWXYZABCDEFG");
-      });
-
-  });
-
-//DESCIFRAR//
-  describe('cipher.decode', () => {
-
-    it('debería ser una función', () => {
-      assert.equal(typeof cipher.decode, 'function');
-    });
-
-    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33');
-  });
-
+describe('cipher.decode', () => {
+ it('debería ser una función', () => {
+   assert.equal(typeof cipher.decode, 'function');
+ });
+ it("debería retornar 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' para 'HIJKLMNOPQRSTUVWXYZABCDEFG' con offest 33", () => {
+   assert.equal(cipher.decode(33,"HIJKLMNOPQRSTUVWXYZABCDEFG"), "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+ });
+ it('debería retornar "H  I" para "A  B " con offest 33', () => {
+ assert.equal(cipher.decode(33,"H I"),"A B");
+ });
 });
